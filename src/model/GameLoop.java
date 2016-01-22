82650e5751259bb13b4b06e9d9e0480f3a51033f
Mohamed Gamal (Jimmy) <jimadness12@gmail.com>
2016-01-22 20:26:45 +0200

    Implemented transitions between stages (Cleaned up the code).

--- src/model/GameLoop.java
+++ src/model/GameLoop.java
@@ -17,69 +17,30 @@ public class GameLoop extends AnimationTimer {
     private GameLoop() {
     }
 
-    private void handleTransitions() throws IndexOutOfBoundsException {
-        switch (Maze.getCurrentNode().getData().getStageType()) {
-            case THREEPATHS:
-                if (Player.getInstance().getPosX() <= Maze.getMinX() && Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal())!= null) {     //leftmost
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosX(Maze.getMaxX()-1);
-                } else if (Player.getInstance().getPosX() >= Maze.getMaxX() && Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()) != null) {   //rightmost
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosX(Maze.getMinX()+1);
-                } else if (Player.getInstance().getPosY() >= Maze.getMinY() && Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()) != null) {   //down
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMaxY()+1);
-                } else if (Player.getInstance().getPosY() <= Maze.getMaxY() && Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()) != null) {    //up
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMinY()-1);
-                }
-                break;
-            case LEFTPATH:
-                if (Player.getInstance().getPosY() >= Maze.getMinY() && Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()) != null) {   //down
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMaxY()+1);
-                } else if (Player.getInstance().getPosY() <= Maze.getMaxY() && Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()) != null) {    //up
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMinY()-1);
-                } else if (Player.getInstance().getPosX() >= Maze.getMaxX() && Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()) != null) {  //right
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosX(Maze.getMinX()+1);
-                }
-                break;
-            case RIGHTPATH:
-                if (Player.getInstance().getPosY() >= Maze.getMinY() && Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()) != null) {   //down
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMaxY()+1);
-                } else if (Player.getInstance().getPosY() <= Maze.getMaxY() && Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()) != null) {    //up
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosY(Maze.getMinY()-1);
-                } else if (Player.getInstance().getPosX() <= Maze.getMinX() && Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal())!=null) {   //leftmost
-                    Maze.getCurrentNode().getData().stop();
-                    Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal()));
-                    Maze.getCurrentNode().getData().start();
-                    Player.getInstance().setPosX(Maze.getMaxX()-1);
-                }
-                break;
+    private void handleTransitions(){
+        if (Player.getInstance().getPosX() <= 0 && Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal()) != null) {
+            Maze.getCurrentNode().getData().stop();
+            Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.LEFT.ordinal()));
+            Maze.getCurrentNode().getData().start();
+            Player.getInstance().setPosX(Maze.getMaxX() - 1);
+        } else if (Player.getInstance().getPosX() >= 832 && Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()) != null) {
+            Maze.getCurrentNode().getData().stop();
+            Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.RIGHT.ordinal()));
+            Maze.getCurrentNode().getData().start();
+            Player.getInstance().setPosX(Maze.getMinX() + 1);
+        } else if (Player.getInstance().getPosY() >= 502 && Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()) != null) {
+            Maze.getCurrentNode().getData().stop();
+            Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.DOWN.ordinal()));
+            Maze.getCurrentNode().getData().start();
+            Player.getInstance().setPosY(Maze.getMaxY() + 1);
+        } else if (Player.getInstance().getPosY() <= 108 && Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()) != null) {
+            Maze.getCurrentNode().getData().stop();
+            Maze.setCurrentNode(Maze.getCurrentNode().getChildren().get(Path.UP.ordinal()));
+            Maze.getCurrentNode().getData().start();
+            Player.getInstance().setPosY(Maze.getMinY() - 1);
         }
     }
+
     @Override
     public void handle(long now) {
         Game.getInstance().makePlayerMovement();
