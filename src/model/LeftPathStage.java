bd6697cd170cb6465a9e74161caf614b51247c64
Mohamed Gamal (Jimmy) <jimadness12@gmail.com>
2016-01-22 20:31:46 +0200

    Converted image objects to static for memory optimization.

--- src/model/LeftPathStage.java
+++ src/model/LeftPathStage.java
@@ -6,12 +6,13 @@ import javafx.scene.image.Image;
 public class LeftPathStage implements GameStage{
     private int ID=0;
     private Path parentCameFrom;
+    private static Image stageImage = new Image("design/LeftPath.jpg");
     public void start(){
         Maze.setMinX(400);
         Maze.setMinY(502);
         Maze.setMaxX(832);
         Maze.setMaxY(108);
-        Game.getInstance().getFrame().getGraphicsContext2D().drawImage(new Image("design/LeftPath.jpg"), 0, 0, Game.getInstance().getFrame().getWidth(), Game.getInstance().getFrame().getHeight());
+        Game.getInstance().getFrame().getGraphicsContext2D().drawImage(stageImage, 0, 0, Game.getInstance().getFrame().getWidth(), Game.getInstance().getFrame().getHeight());
         Maze.getCurrentNode().initialize();
     }
     public LeftPathStage(Path parentCameFrom){
